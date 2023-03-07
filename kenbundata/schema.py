3dad764286d592e8648710172b57f43443e7d4f0
osoken <osoken@osoekawalab.com>
2023-03-08 00:24:41 +0900

    feat(schema): add dict

--- kenbundata/schema.py
+++ kenbundata/schema.py
@@ -1,3 +1,6 @@
+from collections.abc import Mapping
+from typing import AbstractSet, Any, Dict, Optional, Union
+
 from pydantic import BaseModel as _BaseModel
 from pydantic import Field
 
@@ -25,3 +28,36 @@ class BaseModel(_BaseModel):
         allow_mutation = False
         alias_generator = Camelizer(["url", "ip"])
         allow_population_by_field_name = True
+
+    def dict(
+        self,
+        *,
+        include: Union[AbstractSet[Union[int, str]], Mapping[Union[int, str], Any], None] = None,
+        exclude: Union[AbstractSet[Union[int, str]], Mapping[Union[int, str], Any], None] = None,
+        by_alias: bool = True,
+        skip_defaults: Optional[bool] = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+    ) -> Dict[str, Any]:
+        """serialize returns a dict representation of the model.
+        >>> from unittest.mock import patch
+        >>> class MyModel(BaseModel):
+        ...     name: str
+        ...     base_url: str
+        ...     two_words: str
+        ...
+        >>> with patch.object(MyModel.__fields__["id"], "default_factory", return_value=Id("T3HW7dZ5SjCtODQLQkY8eA")):
+        ...     MyModel(name="kenbun", base_url="https://kenbun.app", two_words="aaa").dict()
+        ...
+        {'id': Id('T3HW7dZ5SjCtODQLQkY8eA'), 'name': 'kenbun', 'baseURL': 'https://kenbun.app', 'twoWords': 'aaa'}
+        """
+        return super(BaseModel, self).dict(
+            include=include,
+            exclude=exclude,
+            by_alias=by_alias,
+            skip_defaults=skip_defaults,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+        )
