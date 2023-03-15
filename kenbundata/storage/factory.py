a5ddc27e5e0882ed31de730b36b8764a4abbdbc6
osoken <osoken@osoekawalab.com>
2023-03-16 00:30:16 +0900

    refactor(storage.factory): use from_settings function

--- kenbundata/storage/factory.py
+++ kenbundata/storage/factory.py
@@ -6,6 +6,6 @@ from .local import LocalStorage, LocalStorageSettings
 def create_storage(settings: GlobalSettings) -> BaseStorage:
     if settings.storage_type == StorageType.LOCAL:
         local_settings = LocalStorageSettings.create_from_global_settings(settings=settings)
-        return LocalStorage(path=local_settings.path)
+        return LocalStorage.from_settings(settings=local_settings)
     else:
         raise ValueError(f"Unknown storage type: {settings.storage_type}")
--- tests/storage/test_factory.py
+++ tests/storage/test_factory.py
@@ -6,7 +6,11 @@ from kenbundata.storage.factory import create_storage
 
 def test_create_storage_local(mocker: MockerFixture) -> None:
     settings = GlobalSettings(storage_type=StorageType.LOCAL, storage_settings={"path": "/tmp"})
-    LocalStorage = mocker.patch("kenbundata.storage.factory.LocalStorage")
+    create_from_global_settings = mocker.patch(
+        "kenbundata.storage.factory.LocalStorageSettings.create_from_global_settings"
+    )
+    from_settings = mocker.patch("kenbundata.storage.factory.LocalStorage.from_settings")
     storage = create_storage(settings=settings)
-    LocalStorage.assert_called_once_with(path="/tmp")
-    assert storage == LocalStorage.return_value
+    from_settings.assert_called_once_with(settings=create_from_global_settings.return_value)
+    create_from_global_settings.assert_called_once_with(settings=settings)
+    assert storage == from_settings.return_value
