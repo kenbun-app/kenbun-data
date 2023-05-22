59002f859030d97ac64202fadc971476bce66832
osoken <osoken@osoekawalab.com>
2023-05-22 23:49:47 +0900

    feat(storage): add get_url_by_id method

--- kenbundata/storage/postgres/core.py
+++ kenbundata/storage/postgres/core.py
@@ -1,7 +1,11 @@
-from collections.abc import Generator
+from collections.abc import Generator, Iterable
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session
+
+from kenbundata.fields import Id
+from kenbundata.types import Blob, Screenshot, Url
 
 from ...fields import Id
 from ...types import Url
@@ -16,11 +20,14 @@ class PostgresStorage(BaseStorage):
         super(PostgresStorage, self).__init__()
         self._postgres_dsn = postgres_dsn
         self._engine = create_engine(self._postgres_dsn)
-        self._session_maker = sessionmaker(bind=self._engine)
 
     @property
-    def session(self) -> Generator[Session, None, None]:
-        yield self._session_maker()
+    def engine(self) -> Engine:
+        return self._engine
+
+    @property
+    def session(self) -> Session:
+        return Session(self.engine)
 
     @classmethod
     def from_settings(cls, settings: BaseStorageSettings) -> "PostgresStorage":
@@ -32,4 +39,23 @@ class PostgresStorage(BaseStorage):
 
     def get_url_by_id(self, id: Id) -> Url:
         with self.session as sess:
-            return Url.from_orm(sess.query(models.Url).filter(models.Url.id == id).first())
+            obj = sess.query(models.Url).filter(models.Url.id == id.uuid).first()
+            return Url(id=Id(obj.id), url=obj.url)
+
+    def get_blob_by_id(self, id: Id) -> Blob:
+        raise NotImplementedError()
+
+    def get_screenshot_by_id(self, id: Id) -> Screenshot:
+        raise NotImplementedError()
+
+    def list_urls(self) -> Iterable[Url]:
+        raise NotImplementedError()
+
+    def store_blob(self, blob: Blob) -> None:
+        raise NotImplementedError()
+
+    def store_screenshot(self, screenshot: Screenshot) -> None:
+        raise NotImplementedError()
+
+    def store_url(self, url: Url) -> None:
+        raise NotImplementedError()
